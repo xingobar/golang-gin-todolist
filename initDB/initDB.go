6b774f44d826a71ae4b536949c1bff5ae679fb8c
xingobar <gary840227@gmail.com>
2020-08-30 23:47:18 +0800

    gorm open args change

--- initDB/initDB.go
+++ initDB/initDB.go
@@ -9,7 +9,7 @@ var Db *gorm.DB
 
 func init() {
 	var err error
-	Db, err = gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/gin_todo")
+	Db, err = gorm.Open("mysql", "root:@/gin_todo")
 	if err != nil {
 		log.Panicln("err: ", err.Error())
 	}
