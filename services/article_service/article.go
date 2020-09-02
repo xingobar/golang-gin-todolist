c14fb711623c8eb416e4d46f9ece9320ebbab126
xingobar <gary840227@gmail.com>
2020-09-02 22:39:27 +0800

    刪除不需要的城市

--- services/article_service/article.go
+++ services/article_service/article.go
@@ -1,7 +1,6 @@
 package article_service
 
 import (
-	"fmt"
 	"golang-gin-todolist/models"
 )
 
@@ -17,25 +16,10 @@ func NewArticleService() *ArticleService {
 	}
 }
 
+// 新增文章
 func (s *ArticleService) Create(article models.Article, tags []models.Tag) bool {
-
 	if err := s.article.Create(article, tags); err != nil {
-		fmt.Println(err.Error())
 		return false
 	}
-
-	//fmt.Println("id: ", article.ID)
-	////tag, _ := s.tag.GetById(1)
-	//err := s.article.AddTags(&article, models.Tag{})
-	//fmt.Println("err =====", err)
-
-	//if err := s.article.AddTags(&article, tags); err != nil {
-	//	fmt.Println(err.Error())
-	//	return false
-	//}
-	//fmt.Println("===============")
-	//fmt.Println(article.ID)
-	//fmt.Println(s.article.AddTags(a, tags))
-
 	return true
 }
