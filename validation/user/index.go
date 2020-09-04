41dd5736435c4a4989751b12fb553bfe3e7e0083
xingobar <gary840227@gmail.com>
2020-09-05 03:40:10 +0800

    register form validation complete

--- validation/user/index.go
+++ validation/user/index.go
@@ -5,13 +5,18 @@ var Message = map[string]string{
 	"Username.required": "請輸入用戶姓名",
 	"Email.required": "請輸入電子郵件",
 	"Password.required": "請輸入密碼",
+	"Password.min": "密碼不得小於6位數",
+	"Email.email": "電子郵件格式不符",
+	"ConfirmPassword.eqcsfield": "密碼不一致",
 }
 
 // 註冊 validation
 type RegisterValidation struct {
 	Username string `form:"username" json:"username" binding:"required"`
-	Email string `form:"email" json:"email" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
+	Email string `form:"email" json:"email" binding:"required,email"`
+	Password string `form:"password" json:"password" binding:"required,min=6"`
+	ConfirmPassword string `form:"password" json:"password" binding:"eqcsfield=RegisterValidation.Password"`
+
 }
 
 
