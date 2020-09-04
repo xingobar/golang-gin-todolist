c926fc8978ef77f9db2a9e4c02ac454b50a442b2
xingobar <gary840227@gmail.com>
2020-09-05 03:17:23 +0800

    form validation 調整

--- validation/user/index.go
+++ validation/user/index.go
@@ -8,25 +8,36 @@ type RegisterValidation struct {
 	Password string `form:"password" json:"password" binding:"required"`
 }
 
+var Message = map[string]string{
+	"Username.required": "請輸入用戶姓名",
+	"Email.required": "請輸入電子郵件",
+	"Password.required": "請輸入密碼",
+}
+
 func (r *RegisterValidation) GetError(err validator.ValidationErrors) string {
 
 	for _, item := range err {
-		if item.Field() == "Username" {
-			switch item.Tag() {
-				case "required":
-					return "請輸入用戶姓名"
-			}
-		} else if item.Field() == "Email" {
-			switch item.Tag() {
-				case "required":
-					return "請輸入電子郵件"
-			}
-		} else if item.Field() == "Password" {
-			switch item.Tag() {
-				case "required":
-					return "請輸入密碼"
-			}
+
+		if val, ok := Message[item.Field() + "." + item.Tag()]; ok {
+			return val
 		}
+
+		//if item.Field() == "Username" {
+		//	switch item.Tag() {
+		//		case "required":
+		//			return "請輸入用戶姓名"
+		//	}
+		//} else if item.Field() == "Email" {
+		//	switch item.Tag() {
+		//		case "required":
+		//			return "請輸入電子郵件"
+		//	}
+		//} else if item.Field() == "Password" {
+		//	switch item.Tag() {
+		//		case "required":
+		//			return "請輸入密碼"
+		//	}
+		//}
 	}
 	return "參數錯誤"
 }
