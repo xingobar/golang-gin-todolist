499a8011ad97379fe03de315395385d892ef5f59
xingobar <gary840227@gmail.com>
2020-09-06 22:18:30 +0800

    change user response resource

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator"
 	"github.com/ulule/deepcopier"
 	"golang-gin-todolist/jwt"
 	"golang-gin-todolist/models"
@@ -11,7 +10,6 @@ import (
 	"golang-gin-todolist/pkg/util"
 	resources2 "golang-gin-todolist/resources"
 	"golang-gin-todolist/services/user_service"
-	"golang-gin-todolist/validation"
 	"golang-gin-todolist/validation/user"
 	"net/http"
 )
@@ -32,10 +30,7 @@ func (c *userController) Register(ctx *gin.Context) {
 	var v user.RegisterValidation
 
 	if err := ctx.ShouldBind(&v); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.INVALID_REQUEST,
-			"msg": validation.GetError(err.(validator.ValidationErrors), user.Message),
-		})
+		resources2.NoValidResponse(ctx, err, user.Message)
 		return
 	}
 
@@ -49,35 +44,23 @@ func (c *userController) Register(ctx *gin.Context) {
 	// 檢查帳號是否存在
 	ok, err := c.service.CheckExistByEmail(v.Email)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.INVALID_REQUEST,
-			"msg": e.GetMsg(e.INVALID_REQUEST),
-		})
+		resources2.ErrorResponse(ctx, http.StatusBadRequest, e.INVALID_REQUEST)
 		return
 	}
 
 	// 帳號存在
 	if !ok {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.EXISTS_EMAIL,
-			"msg": e.GetMsg(e.EXISTS_EMAIL),
-		})
+		resources2.ErrorResponse(ctx, http.StatusBadRequest, e.EXISTS_EMAIL)
 		return
 	}
 
 	// 註冊
 	if ok := c.service.Register(user); !ok {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.INVALID_REQUEST,
-			"msg": e.GetMsg(e.INVALID_REQUEST),
-		})
+		resources2.ErrorResponse(ctx, http.StatusBadRequest, e.INVALID_REQUEST)
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"code": e.SUCCESS,
-		"msg": e.GetMsg(e.SUCCESS),
-	})
+	resources2.SuccessResponse(ctx, e.GetMsg(e.SUCCESS))
 }
 
 // 登入
@@ -85,48 +68,33 @@ func (c *userController) Login(ctx *gin.Context) {
 	var login user.LoginValidation
 
 	if err := ctx.ShouldBind(&login); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.INVALID_REQUEST,
-			"msg": validation.GetError(err.(validator.ValidationErrors), user.Message),
-		})
+		resources2.NoValidResponse(ctx, err, user.Message)
 		return
 	}
 
 	user, err := c.service.GetUserByEmail(login.Email)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.LOGIN_ERROR,
-			"msg": e.GetMsg(e.LOGIN_ERROR),
-		})
+		resources2.ErrorResponse(ctx, http.StatusBadRequest, e.LOGIN_ERROR)
 		return
 	}
 
 	// 檢查密碼是否一致
 	if ok := util.ComparePasswords(user.Password, []byte(login.Password)); !ok {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.LOGIN_ERROR,
-			"msg": e.GetMsg(e.LOGIN_ERROR),
-		})
+		resources2.ErrorResponse(ctx, http.StatusBadRequest, e.LOGIN_ERROR)
 		return
 	}
 
 	id := int(user.ID)
 	token, err := jwt.CreateJwtToken(id)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.TOKEN_ERROR,
-			"msg": e.GetMsg(e.TOKEN_ERROR),
-		})
+		resources2.ErrorResponse(ctx, http.StatusBadRequest, e.TOKEN_ERROR)
 		return
 	}
 
 	// 把token 存到快取
 	saveErr := cache.SetTokenCache(id, token)
 	if saveErr != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.CACHE_ERROR,
-			"msg": e.GetMsg(e.CACHE_ERROR),
-		})
+		resources2.ErrorResponse(ctx, http.StatusBadRequest, e.CACHE_ERROR)
 		return
 	}
 
@@ -134,10 +102,7 @@ func (c *userController) Login(ctx *gin.Context) {
 	deepcopier.Copy(token).To(resources)
 	deepcopier.Copy(user).To(resources)
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"code": e.SUCCESS,
-		"msg": resources,
-	})
+	resources2.SuccessResponse(ctx, resources)
 }
 
 // 取得會員文章
@@ -145,16 +110,8 @@ func (c *userController) GetArticles (ctx *gin.Context) {
 
 	article, err := c.service.GetArticles(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": e.INVALID_REQUEST,
-			"msg": e.GetMsg(e.INVALID_REQUEST),
-		})
+		resources2.ErrorResponse(ctx, http.StatusBadRequest, e.INVALID_REQUEST)
 		return
 	}
-
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"code": e.SUCCESS,
-		"msg": article,
-	})
+	resources2.SuccessResponse(ctx, article)
 }
--- validation/index.go
+++ validation/index.go
@@ -4,7 +4,7 @@ import "github.com/go-playground/validator"
 
 func GetError(err validator.ValidationErrors, message map[string]string) string {
 	for _, item := range err {
-		// form field + form validation tag
+		// struct column + form validation tag
 		if val, ok := message[item.Field() + "." + item.Tag()]; ok {
 			return val
 		}
