525e57865ffeeff63f3c23e31417197d9575860e
xingobar <gary840227@gmail.com>
2020-09-06 10:06:34 +0800

    edit tag struct field

--- models/tag.go
+++ models/tag.go
@@ -7,7 +7,7 @@ import (
 type Tag struct {
 	ID uint `gorm:"primaryKey;autoIncrement" json:"id"`
 	Title string `json:"title"`
-	Articles []Article `gorm:"many2many:article_tags;" json:"articles"`
+	Articles []Article `gorm:"many2many:article_tags;" json:"articles,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time	`json:"updated_at"`
 	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
